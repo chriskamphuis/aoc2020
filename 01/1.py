2518c7b72bdbf1eedf0005d05dba59110bfed354
chriskamphuis <mail@chriskamphuis.com>
2020-12-01 11:55:39 +0100

    small optimization day 1

--- 01/1.py
+++ 01/1.py
@@ -1,6 +1,9 @@
+numbers = set()
 with open('input.txt', 'r') as f:
-    numbers = {int(line) for line in f}
-for n in list(numbers):
-    if 2020 - n in numbers:
-        print(n * (2020 - n))
-        break
+    for line in f: 
+        n = int(line)
+        if 2020 - n in numbers:
+            print(n*(2020 - n))
+            break
+        else:
+            numbers |= {n}
--- 01/2.py
+++ 01/2.py
@@ -1,8 +1,8 @@
 import sys
 with open('input.txt', 'r') as f:
     numbers = {int(line) for line in f}
-for n in list(numbers):
-    for k in list(numbers):
+for n in numbers:
+    for k in numbers:
         if 2020 - n - k in numbers:
             print(n * (2020 - n - k) * k)
             sys.exit()
