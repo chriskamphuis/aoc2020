7613509856aca30c6ff5d9941c9bf23da37c9ebf
chriskamphuis <mail@chriskamphuis.com>
2020-12-03 11:11:46 +0100

    change x and y, and clean up

--- 03/1.py
+++ 03/1.py
@@ -1,16 +1,13 @@
-trees = []
 with open('input.txt', 'r') as f:
-    for line in f:
-        trees.append([True if v == '#' else False for v in line.strip()])
+    trees = [[1 if v == '#' else 0 for v in line.strip()] for line in f]
 
 forest_length = len(trees[0])
 x = y = 0
-slope_x = 1
-slope_y = 3
+slope_y = 1
+slope_x = 3
 counter = 0
-while x < len(trees):
-    if trees[x][y]:
-        counter+=1
-    x += slope_x
-    y = (y + slope_y) % forest_length
+while y < len(trees):
+    counter += trees[y][x]
+    y += slope_y
+    x = (x + slope_x) % forest_length
 print(counter)
--- 03/2.py
+++ 03/2.py
@@ -1,20 +1,16 @@
-trees = []
 with open('input.txt', 'r') as f:
-    for line in f:
-        trees.append([True if v == '#' else False for v in line.strip()])
+    trees = [[1 if v == '#' else 0 for v in line.strip()] for line in f]
 
 forest_length = len(trees[0])
-
 counts = 1
-
 slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-for slope_y, slope_x in slopes:
+
+for slope_x, slope_y in slopes:
     x = y = 0 
     counter = 0
-    while x < len(trees):
-        if trees[x][y]:
-            counter+=1
-        x += slope_x
-        y = (y + slope_y) % forest_length
+    while y < len(trees):
+        counter+=trees[y][x]
+        y += slope_y
+        x = (x + slope_x) % forest_length
     counts *= counter
 print(counts)
