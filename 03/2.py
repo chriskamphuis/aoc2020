6cf701abfd044478e9208f42ce882b74faa65223
chriskamphuis <mail@chriskamphuis.com>
2020-12-03 11:13:08 +0100

    pep8

--- 03/2.py
+++ 03/2.py
@@ -6,10 +6,9 @@ counts = 1
 slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 
 for slope_x, slope_y in slopes:
-    x = y = 0 
-    counter = 0
+    counter = x = y = 0 
     while y < len(trees):
-        counter+=trees[y][x]
+        counter += trees[y][x]
         y += slope_y
         x = (x + slope_x) % forest_length
     counts *= counter
