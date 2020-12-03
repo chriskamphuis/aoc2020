c3f0c7027f13f3a1351d23f51503dc46535ee935
chriskamphuis <mail@chriskamphuis.com>
2020-12-03 10:10:05 +0100

    day 3

---
+++ 03/1.py
@@ -0,0 +1,17 @@
+from pprint import pprint
+trees = []
+with open('input.txt', 'r') as f:
+    for line in f:
+        trees.append([True if v == '#' else False for v in line.strip()])
+
+forest_length = len(trees[0])
+x = y = 0
+slope_x = 1
+slope_y = 3
+counter = 0
+while x < len(trees):
+    if trees[x][y]:
+        counter+=1
+    x += slope_x
+    y = (y + slope_y) % forest_length
+print(counter)
---
+++ 03/2.py
@@ -0,0 +1,21 @@
+from pprint import pprint
+trees = []
+with open('input.txt', 'r') as f:
+    for line in f:
+        trees.append([True if v == '#' else False for v in line.strip()])
+
+forest_length = len(trees[0])
+
+counts = 1
+
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+for slope_y, slope_x in slopes:
+    x = y = 0 
+    counter = 0
+    while x < len(trees):
+        if trees[x][y]:
+            counter+=1
+        x += slope_x
+        y = (y + slope_y) % forest_length
+    counts *= counter
+print(counts)
