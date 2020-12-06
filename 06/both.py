2808e8012477da5e168bfdbd1647872a8d980e1f
chriskamphuis <mail@chriskamphuis.com>
2020-12-06 12:09:17 +0100

    day 6 one line

--- 06/both.py
+++ 06/both.py
@@ -1,2 +1,1 @@
-for f in [set.union, set.intersection]:
-    print(sum([len(f(*[set(x) for x in l.split('\n')])) for l in open('input.txt').read().split('\n\n')]))
+[print(sum([len(f(*[set(x) for x in l.split('\n')])) for l in open('input.txt').read().split('\n\n')])) for f in [set.union, set.intersection]]
