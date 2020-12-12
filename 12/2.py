8e93986ce262782293eb1ebcb03eb58d4645f862
chriskamphuis <mail@chriskamphuis.com>
2020-12-12 15:52:09 +0100

    clean up

--- 12/2.py
+++ 12/2.py
@@ -4,6 +4,8 @@ waypoint = [10, 1] # 'EN' (west and south are negative)
 directions = [[1, 0], [0, -1], [-1, 0], [0, 1]]
 direction_map = {d:i for d, i in zip('ESWN', directions)}
 loc = [0, 0]
+rotation = np.array([[0, -1], [1, 0]])
+
 
 for line in open('input.txt'):
     action = line[0]
@@ -17,7 +19,5 @@ for line in open('input.txt'):
         value = -value + 360
         action = 'R'
     if action == 'R':
-        for _ in range(value//90):
-            waypoint = np.array(waypoint) @ np.array([[0, -1], [1, 0]])
-            waypoint = list(waypoint)
+        waypoint = list(np.array(waypoint) @ rotation**(value//90))
 print(sum([abs(x) for x in loc]))
