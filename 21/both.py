a7e073fe374e490fbb733125731704438a42669a
chriskamphuis <mail@chriskamphuis.com>
2020-12-21 11:43:42 +0100

    day 21

---
+++ 21/both.py
@@ -0,0 +1,55 @@
+from collections import defaultdict
+
+allergen_ingredients_map = defaultdict(list)
+ingredients_allergen_map = defaultdict(set)
+all_ingredients = set()
+all_ingredients_list = list()
+all_allergens = set()
+
+with open('input.txt', 'r') as f:
+    for line in f:
+        line = line.strip()[:-1]
+        ingredients, allergens = line.split(' (contains ')
+        ingredients = ingredients.split(' ')
+        allergens = allergens.split(', ')
+        all_ingredients |= set(ingredients)
+        for allergen in allergens:
+            allergen_ingredients_map[allergen].append(ingredients)
+            all_allergens.add(allergen)
+        all_ingredients_list.append(ingredients)
+
+valid = {i for i in all_ingredients}
+invalid = set()
+for allergen, ingredients in allergen_ingredients_map.items():
+    start = set(ingredients[0])
+    for ingredient in ingredients[1:]:
+        start &= set(ingredient) 
+    for ingredient in start:    
+        ingredients_allergen_map[ingredient].add(allergen) 
+    invalid |= start
+
+valid ^= invalid
+count = 0
+for all_ingredients in all_ingredients_list:
+    for ingredient in all_ingredients:
+        if ingredient in valid:
+            count += 1
+# Part 1
+print(count)
+todo = {e for e in all_allergens}
+
+done = []
+while True:
+    for k, v in ingredients_allergen_map.items():
+        if len(v) == 1:
+            found = v.pop()
+            done.append([k, found])
+            break
+    else:
+        break
+    del ingredients_allergen_map[k]
+    for k,v, in ingredients_allergen_map.items():
+        v.discard(found)
+
+# Part 2
+print(','.join([e[0] for e in sorted(done, key=lambda x: x[1])]))
